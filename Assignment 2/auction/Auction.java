0f45e2cd414f7b185a58f9af2da5ff8176133901
dharshatharan <dharshatharan@gmail.com>
2019-10-05 22:13:12 -0400

    assignment 2 complete

--- Assignment 2/auction/Auction.java
+++ Assignment 2/auction/Auction.java
@@ -10,8 +10,8 @@ import java.util.*;
  * @author (of AuctionSkeleton) Lynn Marshall
  * @version 2.0
  * 
- * @author <YOUR NAME HERE>
- * @version <THE DATE AND HERE>
+ * @author Dharshatharan J. A.
+ * @version 1.0
  * 
  */
 public class Auction
@@ -27,29 +27,23 @@ public class Auction
      */
     private int nextLotNumber;
     
-    private boolean auctionOpen;
-
-    // You need to add something here.  (Hint: an auction may be open or closed.)
-
     /**
-     * Create a new auction.
+     * Keeps track if the auction is open or closed
+     */
+    private boolean auctionOpen;
+    
+    /**
+     * Create a new auction
      */
     public Auction()
     {
         lots = new ArrayList<Lot>();
         nextLotNumber = 1;
         auctionOpen = true;
-        // you need to add something here -- see hint above.
     }
     
     /**
-     * Add a second constructor here.  This constructor takes
-     * an Auction as a parameter.  Provided the auction parameter
-     * is closed, the constructor creates a new auction containing
-     * the unsold lots of the closed auction.  If the auction parameter
-     * is still open or null, this constructor behaves like the
-     * default constructor.
-     * (You need to write the entire method.)
+     * Create an auction with the unsold items of a previously closed auction
      */
     public Auction(Auction auction){
         if(!auction.auctionOpen){
@@ -75,8 +69,6 @@ public class Auction
     /**
      * Enter a new lot into the auction.  Returns false if the
      * auction is not open or if the description is null.
-     * (You need to add the return type, update the documentation, 
-     * and change the code.)
      *
      * @param description A description of the lot.
      */
@@ -92,10 +84,6 @@ public class Auction
 
     /**
      * Show the full list of lots in this auction.
-     *
-     * Print a blank line first, to make our output look nicer. 
-     * If there are no lots, print a message indicating this 
-     * (You need to update the code given below.)
      */
     public void showLots()
     {
@@ -111,22 +99,12 @@ public class Auction
     
     /**
      * Bid for a lot.
-     * Do not assume that the lots are stored in numerical order.
-     * Prints a message indicating whether the bid is successful or not.
-     *   
-     * First print a blank line.  
-     * Then print whether or not the bid is successful.
-     * If the bid is successful, also print the
-     * lot number, high bidder's name, and the bid value.
-     * If the bid is not successful, also print the lot number 
-     * and high bid (but not the high bidder's name).
      * 
      * Returns false if the auction is closed, the lot doesn't
      * exist, the bidder is null, or the bid was not positive
      * and true otherwise (even if the bid was not high enough).
-     * (You need to update the return type, documentation, and code.)
      *
-     * @param number The lot number being bid for.
+     * @param lotNumber The lot number being bid for.
      * @param bidder The person bidding for the lot.
      * @param value  The value of the bid.
      */
@@ -146,10 +124,8 @@ public class Auction
 
     /**
      * Return the lot with the given number. 
-     * Do not assume that the lots are stored in numerical order.   
      *   
      * Returns null if the lot does not exist.
-     * (You need to update the code.)
      *
      * @param lotNumber The number of the lot to return.
      *
@@ -167,13 +143,8 @@ public class Auction
     
     /**
      * Closes the auction and prints information on the lots.
-     * First print a blank line.  Then for each lot,
-     * its number and description are printed.
-     * If it did sell, the high bidder and bid value are also printed.  
-     * If it didn't sell, print that it didn't sell.
      *
      * Returns false if the auction is already closed, true otherwise.
-     * (You need to update the return type, documentation, and code.)
      */
     public boolean close()
     {
@@ -204,13 +175,9 @@ public class Auction
     /**
      * Remove the lot with the given lot number, as long as the lot has
      * no bids, and the auction is open.  
-     * You must use an Iterator object to search the list and,
-     * if applicable, remove the item.
-     * Do not assume that the lots are stored in numerical order.
      *
      * Returns true if successful, false otherwise (auction closed,
      * lot does not exist, or lot has a bid).
-     * (You need to update the return type, documentation, and code.)
      *
      * @param number The number of the lot to be removed.
      */
